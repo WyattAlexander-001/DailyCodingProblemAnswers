158e334af5530430c01e2890585b03badfee25f9
WyattAlexander-001 <wyattbushman117@gmail.com>
2024-09-12 11:39:16 -0400

    solved one easy problem! 1831

---
+++ DailyCodingProblems/DailyCodingProblemsAnswersJava/src/easy/SmallestPositiveIntThatIsNotSum.java
@@ -0,0 +1,29 @@
+package easy;
+
+/*
+Problem #1831
+This problem was asked by Amazon.
+Given a sorted array, find the smallest positive integer that is not the sum of a subset of the array.
+For example, for the input [1, 2, 3, 10], you should return 7.
+Do this in O(N) time.
+
+*/
+public class SmallestPositiveIntThatIsNotSum {
+
+    private int [] arr;
+    public static int smallestPosInt(int [] arr){
+        int sum = 1;
+        for(int i=0;i<arr.length;i++){
+            if(i > sum){
+                break;
+            }
+            sum += i;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        int [] jim = {1,2,3,10}; //Answer should be 7 but getting 13
+        System.out.println(smallestPosInt(jim));
+    }
+
+}
